82d56dc80779b99a1d7b80bcbc93ca4a2eeb5466
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-05-24 12:28:21 -0300

    configurando o componente homeproducts

---
+++ src/components/home/HomeProducts.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <HeaderPrincipal />
+    <Products />
+  </main>
+</template>
+
+<script>
+import Products from "../products/AllProducts.vue";
+import HeaderPrincipal from "../header/PrincipalHeader.vue";
+
+export default {
+  name: "HomeProducts",
+  components: {
+    Products,
+    HeaderPrincipal,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
