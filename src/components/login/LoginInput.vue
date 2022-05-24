190234f67ae7fc996ab21b6ee37b8c63d0a2ddfa
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-05-24 12:29:05 -0300

    configurando o componente personallogin

---
+++ src/components/login/LoginInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <h2>Welcome</h2>
+  <label for="client_name">
+    <input :v-model="clientName" type="email" id="client_name" placeholder="User Name" />
+  </label>
+  <router-link class="{ disabled: clientName.length < 5 }" to="home"> Login </router-link>
+</template>
+
+<script>
+export default {
+  name: "LoginInput",
+  data() {
+    return {
+      clientName: "",
+    };
+  },
+};
+</script>
+
+<style>
+h2 {
+  font-size: 3vw;
+  color: blue;
+}
+
+a {
+  border: none;
+  border-radius: 10px;
+  font-size: 2vw;
+  text-decoration: none;
+  background: rgb(112, 112, 238);
+  color: rgb(236, 233, 233);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+a:hover {
+  cursor: pointer;
+  background: rgb(187, 91, 88);
+}
+</style>
---
+++ src/components/login/PersonalLogin.vue
@@ -0,0 +1,93 @@
+<template>
+  <section class="login_container">
+    <HeaderLogin :title="title"></HeaderLogin>
+    <form>
+      <LoginInput />
+    </form>
+  </section>
+</template>
+
+<script>
+import LoginInput from "./LoginInput.vue";
+import HeaderLogin from "./HeaderLogin.vue";
+
+export default {
+  name: "PersonalLogin",
+  components: {
+    LoginInput,
+    HeaderLogin,
+  },
+  data() {
+    return {
+      clientName: "",
+      title: "Login",
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.disabled {
+  opacity: 0.5;
+  pointer-events: none;
+}
+
+.login_container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  font-family: Arial, Helvetica, sans-serif;
+  background: linear-gradient(90deg, #b5b4e0, #6c62bb);
+}
+
+.login_container h1 {
+  color: white;
+  font-size: 4rem;
+}
+
+form {
+  width: 50vw;
+  height: 60vh;
+  border: 1px solid black;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  background: rgb(255, 255, 255);
+}
+
+form label {
+  width: 80%;
+  height: 10%;
+  display: flex;
+  justify-content: space-around;
+  font-size: 1.5rem;
+}
+
+form label input {
+  width: 90%;
+  padding-left: 20px;
+  border: none;
+  border-bottom: 2px solid rgb(209 209 212);
+}
+
+form label input:hover {
+  border-bottom-color: rgb(106, 103, 158);
+}
+
+.header_container {
+  position: fixed;
+  transition: left 0.5s linear;
+  color: blue;
+}
+</style>
